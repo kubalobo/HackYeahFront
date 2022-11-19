f422936183d877822f447629d7939a42d27a2883
koallcia <alicja.konkol@gmail.com>
2022-11-19 18:38:29 +0100

    add menu style

--- src/components/Menu.svelte
+++ src/components/Menu.svelte
@@ -1,137 +1,175 @@
 <script>
-    import dashboardIcon from "../assets/icons/dashboard.svg";
-    import entitiesIcon from "../assets/icons/entities.svg";
-    import postsIcon from "../assets/icons/posts.svg";
-    import profileIcon from "../assets/icons/profile.svg";
-    import arrowsIcon from "../assets/icons/arrows.svg";
+  import dashboardIcon from "../assets/icons/dashboard.svg";
+  import entitiesIcon from "../assets/icons/entities.svg";
+  import postsIcon from "../assets/icons/posts.svg";
+  import profileIcon from "../assets/icons/profile.svg";
+  import arrowsIcon from "../assets/icons/arrows.svg";
 
-    import { route } from "../stores/stores";
+  import { route } from "../stores/stores";
 
-    import { fade } from "svelte/transition";
+  import { fade } from "svelte/transition";
 
-    const fadeIn = {
-        delay: 100,
-        duration: 200,
-    };
+  const fadeIn = {
+    delay: 100,
+    duration: 200,
+  };
 
-    const fadeOut = {
-        delay: 0,
-        duration: 100,
-    };
+  const fadeOut = {
+    delay: 0,
+    duration: 100,
+  };
 
-    let activeIcon = "entites";
+  let activeIcon = "entites";
 
-    let isExpanded = false;
+  let isExpanded = false;
 </script>
 
 <nav class:expanded={isExpanded}>
-    <ul>
-        <button on:click={() => ($route = "dashboard")}>
-            <li>
-                <img
-                    src={dashboardIcon}
-                    alt="Dashboard"
-                    class:active={$route === "dashboard"}
-                />
-                {#if isExpanded}
-                    <span in:fade={fadeIn} out:fade={fadeOut}>Inbox</span>
-                {/if}
-            </li>
-        </button>
-        <button on:click={() => ($route = "posts")}>
-            <li>
-                <img
-                    src={postsIcon}
-                    alt="Posts"
-                    class:active={$route === "posts"}
-                />
-                {#if isExpanded}
-                    <span in:fade={fadeIn} out:fade={fadeOut}>Starred</span>
-                {/if}
-            </li>
-        </button>
-        <button on:click={() => ($route = "entities")}>
-            <li>
-                <img
-                    src={entitiesIcon}
-                    alt="Entities"
-                    class:active={$route === "entities"}
-                />
-                {#if isExpanded}
-                    <span in:fade={fadeIn} out:fade={fadeOut}>Entities</span>
-                {/if}
-            </li>
-        </button>
-        <button on:click={() => ($route = "profile")}>
-            <li>
-                <img
-                    src={profileIcon}
-                    alt="Profile"
-                    class:active={$route === "profile"}
-                />
-                {#if isExpanded}
-                    <span in:fade={fadeIn} out:fade={fadeOut}>Profile</span>
-                {/if}
-            </li>
-        </button>
-    </ul>
-    <button on:click={() => (isExpanded = !isExpanded)}>
-        <img src={arrowsIcon} alt="Entities" />
-        <!-- {#if isExpanded}
-            
-        {:else}
-            
-        {/if} -->
+  <ul>
+    <button on:click={() => ($route = "dashboard")}>
+      <li>
+        <img
+          src={dashboardIcon}
+          alt="Dashboard"
+          class:active={$route === "dashboard"}
+        />
+        {#if isExpanded}
+          <span
+            in:fade={fadeIn}
+            out:fade={fadeOut}
+            class:active={$route === "dashboard"}
+          >
+            Inbox
+          </span>
+        {/if}
+      </li>
     </button>
+    <button on:click={() => ($route = "posts")}>
+      <li>
+        <img src={postsIcon} alt="Posts" class:active={$route === "posts"} />
+        {#if isExpanded}
+          <span
+            in:fade={fadeIn}
+            out:fade={fadeOut}
+            class:active={$route === "posts"}
+          >
+            Starred
+          </span>
+        {/if}
+      </li>
+    </button>
+    <button on:click={() => ($route = "entities")}>
+      <li>
+        <img
+          src={entitiesIcon}
+          alt="Entities"
+          class:active={$route === "entities"}
+        />
+        {#if isExpanded}
+          <span
+            in:fade={fadeIn}
+            out:fade={fadeOut}
+            class:active={$route === "entities"}
+          >
+            Entities
+          </span>
+        {/if}
+      </li>
+    </button>
+    <button on:click={() => ($route = "profile")}>
+      <li>
+        <img
+          src={profileIcon}
+          alt="Profile"
+          class:active={$route === "profile"}
+        />
+        {#if isExpanded}
+          <span
+            in:fade={fadeIn}
+            out:fade={fadeOut}
+            class:active={$route === "profile"}
+          >
+            Profile
+          </span>
+        {/if}
+      </li>
+    </button>
+  </ul>
+  <button on:click={() => (isExpanded = !isExpanded)}>
+    <li>
+      <img src={arrowsIcon} alt="Entities" class:rotate={isExpanded} />
+      {#if isExpanded}
+        <span in:fade={fadeIn} out:fade={fadeOut} class:active={true}>
+          Collapse menu
+        </span>
+      {/if}
+    </li>
+  </button>
 </nav>
 
-<style>
-    nav {
-        grid-area: nav;
-        height: 100vh;
-        background-color: #324754;
-        color: #a2b7c4;
-        transition: ease-out 200ms;
-        width: 60px;
-        overflow: hidden;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-    }
+<style lang="scss">
+  nav {
+    grid-area: nav;
+    height: 100vh;
+    transition: ease-out 200ms;
+    width: 60px;
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    background: linear-gradient(150deg, $primary 0%, $lightBlue 100%);
+    box-shadow: 0px 3px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 0px 20px 20px 0px;
+  }
 
-    .expanded {
-        transition: ease-out 200ms;
-        width: 200px;
-    }
+  .expanded {
+    transition: ease-out 200ms;
+    width: 300px;
+  }
 
-    .active {
-        filter: invert(83%) sepia(40%) saturate(1702%) hue-rotate(162deg)
-            brightness(95%) contrast(104%);
-    }
+  .active {
+    filter: invert(83%) sepia(40%) saturate(1702%) hue-rotate(162deg)
+      brightness(95%) contrast(104%);
+  }
 
-    ul {
-        list-style: none;
-        padding: 20px;
-        margin: 0;
-    }
+  .rotate {
+    transform: rotate(-180deg);
+  }
 
-    li {
-        width: 200px;
-    }
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
 
-    button {
-        border: none;
-        background: none;
-        color: #a2b7c4;
-        text-transform: uppercase;
-        text-align: left;
+  li {
+    display: flex;
+    flex-direction: row;
+    column-gap: 20px;
+    width: 200px;
+    height: 24px;
+  }
 
-        background: none;
-        color: inherit;
-        border: none;
-        /* padding: 0; */
-        font: inherit;
-        cursor: pointer;
-        outline: inherit;
-    }
+  img {
+    width: 24px;
+    transition-property: transform;
+    transition-duration: 0.5s;
+  }
+  span {
+    text-transform: none;
+    color: #ffffff;
+    font-size: 16px;
+  }
+
+  button {
+    border: none;
+    background: none;
+    color: #a2b7c4;
+    text-transform: uppercase;
+    text-align: left;
+    padding: 20px 18px;
+    font: inherit;
+    cursor: pointer;
+    outline: inherit;
+  }
 </style>
